e50b8b33dfdecc2e0d0e27c08ead787a8be03579
Josue Zambrano <jjzambranoz@puce.edu.ec>
2024-07-13 10:16:28 -0500

    commit final

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
+def index (request):
+    movies = Movie.objects.order_by('title')
+    template = loader.get_template('index.html')
+    context = {'movies': movies}
+    return HttpResponse(template.render(context, request))
+
+def display_movie (request, movie_id):
+    movie = Movie.objects.get(pk = movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {'movie': movie}
+    return HttpResponse(template.render(context, request))
